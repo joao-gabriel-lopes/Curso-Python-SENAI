401e6754ec90c1c7a7d1eef717ca3ee7a68ff519
João <jgablopes2008@gmail.com>
2025-10-18 15:43:21 -0300

    Python divisão

---
+++ 18-10/Atividade 4/divisao.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+if(num2 != 0): 
+    print("O resultado da divisão é:", num1 / num2)
+else:
+    print("Não é possível realizar o cálculo")
