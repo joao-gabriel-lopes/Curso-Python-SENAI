25aeb80f9f6bfa3e89ef781914a8179aec58ba44
João <jgablopes2008@gmail.com>
2025-11-01 08:57:51 -0300

    Ajustes

--- 01-11/Atividade1/vaga.py
+++ 01-11/Atividade1/vaga.py
@@ -4,8 +4,6 @@ formacao = input("Você possui formação acadêmica? (sim/nao): ")
 experiencia = int(input("Digite quantos anos de experiência você possui: "))
 
 if (idade >= 18 and experiencia >= 2 and formacao == "sim"):
-    mensagem = f"Candidato {nome}, você foi aprovado, prossiga para o RH para mais instruções"
+    print(f"Candidato {nome}, você foi aprovado, prossiga para o RH para mais instruções")
 else:
-    mensagem = f"Candidato {nome}, você não possui as exigências necessárias, tenha um bom dia"
-
-print(mensagem)
+    print(f"Candidato {nome}, você não possui as exigências necessárias, tenha um bom dia")
