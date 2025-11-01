f0fab581ee45e7004224b2caa016f7d906fbe4b9
João <jgablopes2008@gmail.com>
2025-11-01 08:53:37 -0300

    Ajustes

--- 01-11/Atividade1/desconto.py
+++ 01-11/Atividade1/desconto.py
@@ -2,9 +2,9 @@ valor = int(input("Digite o valor da compra: "))
 
 if(valor >= 200 and valor < 500):
     valor *= 0.85
-    print("O valor da compra com 15% de desconto é:", valor)
+    print("O valor da compra com 15% de desconto é: R${valor}")
 elif(valor >= 500):
     valor *= 0.75
-    print("O valor da compra com 25% de desconto é:", valor)
+    print("O valor da compra com 25% de desconto é: R${valor}")
 else:
-    print("O valor da compra é:", valor)
+    print("O valor da compra é: R${valor}")
