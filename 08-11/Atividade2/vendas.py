51b01cc2bc31cee80a7b3e8d64828239c5fe02a5
João <jgablopes2008@gmail.com>
2025-11-08 13:40:00 -0300

    Ajustes

--- 08-11/Atividade2/vendas.py
+++ 08-11/Atividade2/vendas.py
@@ -1,6 +1,6 @@
 def calcularSalario(valorVendas, anosDeEmpresa):
     if(valorVendas > 50000 and anosDeEmpresa >= 5):
-        mensagem = f"Parabéns! Você recebeu um bônus de 10%, resultando em {(valorVendas * 0.1):.2f}"
+        mensagem = f"Parabéns! Você vendeu {valorVendas} e ganhou uma comissão de {(valorVendas * 0.1):.2f}"
         return mensagem
     else:
         mensagem = f"Você não atingiu os requisitos para o bônus"
